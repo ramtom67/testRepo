495d93e87e2feb1ae90c0dc069d5b1710bc67848
ramtom67 <ram.jr67@gmail.com>
2022-08-24 11:07:25 +0530

    test

---
+++ myPractices_02/myPractices_02/src/com/main/OopsEx01.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import com.oops.Student;
+
+public class OopsEx01 {
+
+	public static void main(String[] args) {
+       
+		Student s1=new Student(111,"Dilip",46,39,45,50,85);
+		
+		s1.CalculateMarks();
+		s1.displayStudentDetails();
+		
+	}
+
+}
